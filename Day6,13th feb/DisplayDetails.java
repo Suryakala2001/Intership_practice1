494710c30358797ee3663723fc9f3464450ee732
unknown <sirisurya13@gmail.com>
2023-02-13 21:32:14 +0530

    done

---
+++ Day6,13th feb/DisplayDetails.java
@@ -0,0 +1,8 @@
+class DispalyDetails{
+  public static void main(String args[]){
+    System.out.println("Name:Siri");
+     System.out.println("Clg:MIT");
+    System.out.println("Branch:CES");
+    System.out.println("Sec:A");
+  }
+}
